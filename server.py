c8eb147bc062189ab22b4ff7cf624b27abbf049b
Stephen Coleman <stephencoleman@Stephens-MacBook-Pro-3.local>
2026-03-17 13:14:19 -0400

    Add boot diagnostics and v3 app bundle

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from deep_translator import GoogleTranslator
 
-APP_VERSION = "2026-03-12.9"
+APP_VERSION = "2026-03-17.1"
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -56,7 +56,7 @@ async def room_debug():
       <script>
         document.getElementById('debug-js').textContent = 'inline-js: ok';
       </script>
-      <script src="/static/app.v2.js?v=20260312-9"></script>
+      <script src="/static/app.v3.js?v=20260317-1"></script>
     </body>
     </html>
     """
